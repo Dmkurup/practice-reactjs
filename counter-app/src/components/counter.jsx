dc468fca2a391c8313b00d2c5ea123a9ef1ee065
Dmkurup <deepthikurupmk@gmail.com>
2020-04-28 18:05:23 +0530

    props and state

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"],
+    value: this.props.value,
   };
 
   renderTags() {
@@ -17,35 +16,40 @@ class Counter extends Component {
     );
   }
   //arrow functions works as binding function for this keyword
-  handleIncrement = (product) => {
-    console.log('Product',product);
-    this.setState({count:this.state.count+1})
+  handleIncrement = () => {
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
     return (
       <div>
+        <h5>Counter #{this.props.id}</h5>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={()=>this.handleIncrement({id:1})}
+          onClick={this.handleIncrement}
         >
           Increment
         </button>
-        {this.renderTags()}
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
       </div>
     );
   }
 
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
