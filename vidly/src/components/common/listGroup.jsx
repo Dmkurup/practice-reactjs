38f5ea324cca2771a075c041d45069268c4b20f0
Dmkurup <deepthikurupmk@gmail.com>
2020-04-30 12:37:47 +0530

    added pagination

---
+++ vidly/src/components/common/listGroup.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { genres } from "../../services/fakeGenreService";
+
+class ListGroup extends Component {
+  render() {
+    const { items, onItemSelect, textProperty, valueProperty } = this.props;
+    return (
+      <ul className="list-group">
+        {items.map((item) => (
+          <li
+            onClick={() => onItemSelect(item)}
+            key={item[valueProperty]}
+            className="list-group-item"
+          >
+            {item[textProperty]}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default ListGroup;
---
+++ vidly/src/components/movies.jsx
@@ -0,0 +1,105 @@
+import React, { Component } from "react";
+import { getMovies, deleteMovie } from "../services/fakeMovieService";
+import Like from "./common/like";
+import Pagination from "./common/pagination";
+import { paginate } from "./utils/paginate";
+import { genres, getGenres } from "../services/fakeGenreService";
+import ListGroup from "./common/listGroup";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+    genres: getGenres(),
+    pageSize: 4,
+    currentPage: 1,
+    currentGenreId: genres[0]._id,
+  };
+
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movie };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
+  };
+
+  handlePageChange = (page) => {
+    console.log("page", page);
+    this.setState({ currentPage: page });
+  };
+
+  handleGenreSelect = (genre) => {
+    console.log("Genre", genre);
+  };
+
+  render() {
+    const { length: moviesCount } = this.state.movies;
+    const { pageSize, currentPage, movies: allMovies, genres } = this.state;
+    if (moviesCount === 0) return <p>There are no movies in the database</p>;
+    const movies = paginate(allMovies, currentPage, pageSize);
+    return (
+      <div className="row">
+        <div className="col-3">
+          <ListGroup
+            items={genres}
+            onItemSelect={this.handleGenreSelect}
+            textProperty="name"
+            valueProperty="_id"
+          />
+        </div>
+        <div className="col">
+          <p>Showing {moviesCount} movies from the data base</p>
+          <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Genre</th>
+                <th>Stock</th>
+                <th>Rate</th>
+                <th>Like</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {movies.map((movie) => (
+                <tr key={movie._id}>
+                  <td>{movie.title}</td>
+                  <td>{movie.genre.name}</td>
+                  <td>{movie.numberInStock}</td>
+                  <td>{movie.dailyRentalRate}</td>
+                  <td>
+                    <Like
+                      liked={movie.liked}
+                      onClick={() => this.handleLike(movie)}
+                    />
+                  </td>
+                  <td>
+                    <button
+                      className="btn btn-danger btn-sm"
+                      onClick={() => this.handleDelete(movie)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <Pagination
+            itemsCount={moviesCount}
+            pageSize={pageSize}
+            onPageChange={this.handlePageChange}
+            currentPage={currentPage}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Movies;
